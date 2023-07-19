262bbcf44c67180feee561528535a41acb1e7d40
Kristian Djura <kristian.djura@hapimag.com>
2023-07-19 14:36:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ button.addEventListener('click', function(){
     let word = document.getElementById('word').value;
     let count = word.length;
     let outputDiv = document.getElementById('output');
-    print("kristian djura")
+    print("line of code")
     outputDiv.innerHTML = `<h1>${count}</h1>`
 });
